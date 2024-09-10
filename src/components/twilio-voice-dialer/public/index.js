ae4563ac24a0c8ffacf35994c4fa3fc110a004fb
Kevin <kchoy@twilio.com>
2024-09-10 15:44:38 -0700

    feat: setToken and recipient

--- src/components/twilio-voice-dialer/public/index.js
+++ src/components/twilio-voice-dialer/public/index.js
@@ -1,1 +1,10 @@
-// TODO: Implement example usage of the Web Component
+(async function () {
+  const identity = '';
+  const response = await fetch(
+    `http://127.0.0.1:3030/token?identity=${identity}`
+  );
+  const data = await response.json();
+
+  const TwilioVoiceDialer = document.querySelector('twilio-voice-dialer');
+  TwilioVoiceDialer.setToken(data.token);
+})();
--- src/components/twilio-voice-dialer/public/twilio-voice-dialer.js
+++ src/components/twilio-voice-dialer/public/twilio-voice-dialer.js
@@ -1,1 +1,51 @@
-// TODO: Implement Web Component
+const render = (x) => `
+  <div class="container">
+    <p id="status">Status: pending</p>
+    <input type="text" placeholder="recipient" id="recipient" value=${x.recipient}>
+  </div>
+`;
+
+class TwilioVoiceDialer extends HTMLElement {
+  static get observedAttributes() {
+    return ['recipient'];
+  }
+
+  device;
+  token;
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+  get recipient() {
+    return this.getAttribute('recipient');
+  }
+
+  connectedCallback() {}
+
+  disconnectedCallback() {
+    this.device.destroy();
+  }
+
+  attributeChangedCallback() {
+    this.shadowRoot.innerHTML = render(this);
+  }
+
+  async handleInit() {
+    this.device = new Twilio.Device(this.token, { logLevel: 1 });
+    this.setStatus('idle');
+  }
+
+  setStatus(status) {
+    const recipientEl = this.shadowRoot.querySelector('#recipient');
+    const statusEl = this.shadowRoot.querySelector('#status');
+    statusEl.innerText = `Status: ${status}`;
+  }
+
+  setToken(token) {
+    this.token = token;
+    this.handleInit();
+  }
+}
+
+customElements.define('twilio-voice-dialer', TwilioVoiceDialer);
