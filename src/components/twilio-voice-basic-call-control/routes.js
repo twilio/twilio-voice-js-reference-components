c1162d66f52b0e08961189bb37f019addfb035df
Kevin Choy <35968892+kpchoy@users.noreply.github.com>
2024-10-28 10:43:12 -0700

    [VBLOCKS-3486] basic security and notes (#17)
    * feat: webhook security validation
    
    * feat: notes and doc links
    
    * refactor: wording

--- src/components/twilio-voice-basic-call-control/routes.js
+++ src/components/twilio-voice-basic-call-control/routes.js
@@ -9,6 +9,8 @@ const client = Twilio(apiKeySid, apiKeySecret, { accountSid });
 router.post('/conferences/:conferenceSid/participants/:callSid', async (req, res) => {
   const { callSid, conferenceSid } = req.params;
 
+  // Update a Participant resource
+  // https://www.twilio.com/docs/voice/api/conference-participant-resource#update-a-participant-resource
   await client
     .conferences(conferenceSid)
     .participants(callSid)
