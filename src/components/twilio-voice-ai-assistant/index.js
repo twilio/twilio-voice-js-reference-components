d44d787b20a1da3b31755f424f45328a922bfd3d
Kevin Choy <kevinchoy22@gmail.com>
2025-08-01 15:14:04 -0700

    refactor: validate ws connection

--- src/components/twilio-voice-ai-assistant/index.js
+++ src/components/twilio-voice-ai-assistant/index.js
@@ -1,8 +1,9 @@
 import WebSocket, { WebSocketServer } from 'ws';
 import OpenAI from 'openai';
 import config from '../../config.js';
+import Twilio from 'twilio';
 
-const { openaiApiKey } = config;
+const { authToken, callbackBaseUrl, openaiApiKey } = config;
 const sessions = new Map();
 const SYSTEM_PROMPT = 'You are a helpful assistant. This conversation is being translated to voice, so answer carefully. When you respond, please spell out all numbers, for example twenty not 20. Do not include emojis in your responses. Do not include bullet points, asterisks, or special symbols.';
 
@@ -19,8 +20,22 @@ const init = (server) => {
     return completion.choices[0].message.content;
   }
 
+  const checkCRelayConnection = (headers) => 
+    Twilio.validateRequest(
+      authToken,
+      headers['x-twilio-signature'],
+      `wss://${callbackBaseUrl}/websocket`,
+      {}
+    );
+
   // Handle WebSocket connections
-  wss.on('connection', (ws) => {
+  wss.on('connection', (ws, req) => {
+    const headers = req.headers;
+    
+    // Validate incoming Twilio requests
+    // https://www.twilio.com/docs/voice/conversationrelay/onboarding#websocket-security-with-signature-validation
+    const isCRelayConnection = checkCRelayConnection(headers);
+
     function broadcastLog(content) {
       const message = JSON.stringify({ type: 'log', content });
       wss.clients.forEach((client) => {
@@ -31,44 +46,56 @@ const init = (server) => {
     }
     broadcastLog('WebSocket connection established');
 
-    ws.on('message', async (data) => {
-      const message = JSON.parse(data);
-      switch (message.type) {
-        case 'setup':
-          const callSid = message.callSid;
-          broadcastLog(`Setup for call: ${callSid}`);
-          ws.callSid = callSid;
-          sessions.set(callSid, [{ role: 'system', content: SYSTEM_PROMPT }]);
-          break;
-        case 'prompt':
-          broadcastLog(`Processing prompt: ${message.voicePrompt}`);
-          const conversation = sessions.get(ws.callSid);
-          conversation.push({ role: 'user', content: message.voicePrompt });
+    function handleMessage() {
+      ws.on('message', async (data) => {
+        const message = JSON.parse(data);
+        switch (message.type) {
+          case 'setup':
+            const callSid = message.callSid;
+            broadcastLog(`Setup for call: ${callSid}`);
+            ws.callSid = callSid;
+            sessions.set(callSid, [{ role: 'system', content: SYSTEM_PROMPT }]);
+            break;
+          case 'prompt':
+            broadcastLog(`Processing prompt: ${message.voicePrompt}`);
+            const conversation = sessions.get(ws.callSid);
+            conversation.push({ role: 'user', content: message.voicePrompt });
 
-          const response = await aiResponse(conversation);
-          conversation.push({ role: 'assistant', content: response });
+            const response = await aiResponse(conversation);
+            conversation.push({ role: 'assistant', content: response });
 
-          ws.send(
-            JSON.stringify({
-              type: 'text',
-              token: response,
-              last: true,
-            })
-          );
-          broadcastLog(`Sent response: ${response}`);
-          break;
-        case 'interrupt':
-          broadcastLog('Handling interruption.');
-          break;
-        default:
-          broadcastLog(`Unknown message type received: ${message.type}`);
-          break;
-      }
-    });
+            ws.send(
+              JSON.stringify({
+                type: 'text',
+                token: response,
+                last: true,
+              })
+            );
+            broadcastLog(`Sent response: ${response}`);
+            break;
+          case 'interrupt':
+            broadcastLog('Handling interruption.');
+            break;
+          default:
+            broadcastLog(`Unknown message type received: ${message.type}`);
+            break;
+        }
+      });
+    }
 
-    ws.on('close', () => {
-      broadcastLog('WebSocket connection closed');
-    });
+    function handleClose() {
+      ws.on('close', () => {
+        broadcastLog('WebSocket connection closed');
+      });
+    }
+
+    if (isCRelayConnection) {
+      handleMessage();
+      handleClose();
+    } else {
+      // This is the browser client. This is where you add your own authentication.
+      handleClose();
+    }
   });
 };
 
